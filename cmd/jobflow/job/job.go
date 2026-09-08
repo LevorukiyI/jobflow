561e47418542fea968ea7dd172cad09daf880b34
LevorukiyI <92181087+LevorukiyI@users.noreply.github.com>
2026-09-09 00:11:44 +0300

    till module 14

---
+++ cmd/jobflow/job/job.go
@@ -0,0 +1,58 @@
+package job
+
+import (
+	"time"
+)
+
+type Job struct {
+	ID        string
+	Type      string
+	Payload   []byte
+	Status    JobStatus
+	CreatedAt time.Time
+}
+
+type JobStatus string
+
+const (
+	StatusPending   JobStatus = "pending"
+	StatusRunning   JobStatus = "running"
+	StatusCompleted JobStatus = "completed"
+	StatusFailed    JobStatus = "failed"
+)
+
+func NewJob(
+	id string,
+	jobType string,
+	payload []byte,
+) Job {
+	return Job{
+		ID:        id,
+		Type:      jobType,
+		Payload:   payload,
+		Status:    StatusPending,
+		CreatedAt: time.Now(),
+	}
+}
+
+func SetStatus(status JobStatus) {
+
+}
+
+func FindJob(
+	jobs []Job,
+	id string,
+) (job *Job) {
+	for i := range jobs {
+		if jobs[i].ID == id {
+			job = &jobs[i]
+			return job
+		}
+	}
+	return nil
+}
+
+func (j Job) IsFinished() bool {
+	return j.Status == StatusCompleted ||
+		j.Status == StatusFailed
+}
--- cmd/jobflow/main.go
+++ cmd/jobflow/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"jobflow/cmd/jobflow/job"
 )
 
 func main() {
 	fmt.Println("test message for go code +==============================")
+	var jobs []job.Job = []job.Job{
+		job.NewJob("job1", "jobType", []byte{}),
+		job.NewJob("job2", "jobType", []byte{}),
+		job.NewJob("job3", "jobType", []byte{}),
+	}
+	for _, job := range jobs {
+		fmt.Println(job.ID)
+	}
+	foundedJob := job.FindJob(jobs, "job2")
+	fmt.Println(foundedJob.ID)
+
 }
